7f2e8bcb89d72696937a611ac9daaa5b4aeda76c
gadepall <gadepall@gmail.com>
2021-10-17 15:34:10 +0530

    Add existing files

--- codes/sevenseg/src/main.cpp
+++ codes/sevenseg/src/main.cpp
@@ -3,25 +3,35 @@
 
 void sevenseg(int a,int b,int c,int d,int e,int f,int g)
 {
-  digitalWrite(36, a); 
-  digitalWrite(39, b); 
-  digitalWrite(34, c); 
-  digitalWrite(35, d); 
-  digitalWrite(32, e); 
-  digitalWrite(33, f);     
-  digitalWrite(25, g); 
+  digitalWrite(32, a); 
+  digitalWrite(33, b); 
+  digitalWrite(25, c); 
+  digitalWrite(26, d); 
+  digitalWrite(27, e); 
+  digitalWrite(14, f);     
+  digitalWrite(12, g); 
 }
 void setup() 
 {
-    pinMode(36, OUTPUT);  
-    pinMode(39, OUTPUT);
-    pinMode(34, OUTPUT);
-    pinMode(35, OUTPUT);
-    pinMode(32, OUTPUT);
+    pinMode(32, OUTPUT);  
     pinMode(33, OUTPUT);
-    pinMode(25, OUTPUT);            
+    pinMode(25, OUTPUT);
+    pinMode(26, OUTPUT);
+    pinMode(27, OUTPUT);
+    pinMode(14, OUTPUT);
+    pinMode(12, OUTPUT);            
 }
 void loop() 
 {
-sevenseg(1,0,0,1,1,1,1);  
+//sevenseg(0,0,0,0,0,0,0);  //0
+//sevenseg(1,0,0,1,1,1,1);  //1
+//sevenseg(0,0,1,0,0,1,0);  //2
+//sevenseg(0,0,0,0,1,1,0);  //3
+//sevenseg(1,0,0,1,1,0,0);  //4
+//sevenseg(0,1,0,0,1,0,0);  //5
+//sevenseg(0,1,0,0,0,0,0);  //6
+//sevenseg(0,0,0,1,1,1,1);  //7
+//sevenseg(0,0,0,0,0,0,0);  //8
+sevenseg(0,0,0,0,1,0,0);  //9
+
 }
